4d0c9cc10302e9d67d4559499ce60b467430a524
Arash Zeinoddini <arash.zeinoddini@gmail.com>
2023-02-21 07:39:13 +0330

    Initial Commit

---
+++ APITracker/APITrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  APITrackerApp.swift
+//  APITracker
+//
+//  Created by Arash Zeinoddini on 2/21/23.
+//
+
+import SwiftUI
+
+@main
+struct APITrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
